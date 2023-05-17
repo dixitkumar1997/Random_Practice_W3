62bbe29aceb407204288e6a28ffaf2e75417c5f3
Dixit kumar <dxtt22@gmail.com>
2023-05-17 06:34:07 +0530

    fifth assignment is done

---
+++ Practice-5(Accordion)/Practice-5.js
@@ -0,0 +1,44 @@
+let count1=0;
+function change1(x){
+    if(count1==0){
+    x.style.display="block";
+    count1=1;
+ } 
+ else if(count1==1){
+    x.style.display="none";
+    count1=0;
+ }
+}
+let count2=0;
+function change2(x){
+    if(count2==0){
+    x.style.display="block";
+    count2=1;
+ } 
+ else if(count2==1){
+    x.style.display="none";
+    count2=0;
+ }
+}
+let count3=0;
+function change3(x){
+    if(count3==0){
+    x.style.display="block";
+    count3=1;
+ } 
+ else if(count3==1){
+    x.style.display="none";
+    count3=0;
+ }
+}
+let count4=0;
+function change4(x){
+    if(count4==0){
+    x.style.display="block";
+    count4=1;
+ } 
+ else if(count4==1){
+    x.style.display="none";
+    count4=0;
+ }
+}
