03a10e7c11cf3da824c805213065e43a61418cd2
Dixit kumar <dxtt22@gmail.com>
2023-05-18 22:46:52 +0530

    sixth assignment is done

---
+++ Practice-6(Horizontal Tab)/Practice-6.js
@@ -0,0 +1,8 @@
+function show(x,y,z,a,b,c){
+ x.style.backgroundColor = "#ddd";
+ y.style.backgroundColor = "gray";
+ z.style.backgroundColor = "gray";
+ a.style.display = "block";
+ b.style.display = "none";
+ c.style.display = "none";
+}
