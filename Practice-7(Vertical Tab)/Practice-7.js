9de08a029081b5020026e20642721e9a0792d1e2
Dixit kumar <dxtt22@gmail.com>
2023-05-19 23:49:01 +0530

    seventh assignment is done

---
+++ Practice-7(Vertical Tab)/Practice-7.js
@@ -0,0 +1,9 @@
+function show(x,y,z,a,b,c){
+    x.style.backgroundColor = "gray";
+    y.style.backgroundColor = "";
+    z.style.backgroundColor = "";
+    a.style.display = "block";
+    b.style.display = "none";
+    c.style.display = "none";
+
+}
